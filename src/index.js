8111153dec395f8d3962c4201e2cb5925f2e27bb
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-04-12 01:29:45 -0400

    install sass

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import $ from 'jquery';
+import './style.scss';
 
 let seconds = 0;
 
 setInterval(() => {
-    seconds = seconds + 1;
-    $('#main').html("You've been on this page for " + seconds + " seconds!");
-}, 1000)
-
+  seconds += 1;
+  $('#main').html(`You have been on this page for ${seconds} seconds!`);
+}, 1000);
