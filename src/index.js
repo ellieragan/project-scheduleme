830097fef4ae8281c0c1eae120c3ab1d238cd459
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-04-12 01:10:46 -0400

    vite and counting

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+import $ from 'jquery';
+
+let seconds = 0;
+
+setInterval(() => {
+    seconds = seconds + 1;
+    $('#main').html("You've been on this page for " + seconds + " seconds!");
+}, 1000)
+
