4f9af44384c5f0ee99cf533020c9651ca60599ed
Chloe <chloe.m.fugle.23@dartmouth.edu>
2023-05-22 21:11:19 -0400

    add loading user events

--- src/components/event.jsx
+++ src/components/event.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 
 function Event(props) {
-  return (
-    <div
-      className="event"
-      id={props.id}
-      data-day={props.day}
-      data-time={props.time}
-      data-block={props.block}
-      data-avail-count={props.availableCount}
-      data-avail={props.available}
-    />
-  );
+  if (props.available.length !== 0) {
+    return (
+      <div
+        className="event"
+        id={props.id}
+        data-day={props.day}
+        data-time={props.time}
+        data-block={props.block}
+        data-avail-count={props.availableCount}
+        data-avail={props.available}
+      >
+        <p className="tooltiptext">{props.available.join(', ')}</p>
+        {/* {Object.entries(props.available).map(([id, name]) => {
+          console.log(name);
+          return (<p className="tooltiptext">{name}</p>);
+        })} */}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className="event"
+        id={props.id}
+        data-day={props.day}
+        data-time={props.time}
+        data-block={props.block}
+        data-avail-count={props.availableCount}
+        data-avail={props.available}
+      />
+    );
+  }
 }
 
 export default Event;
