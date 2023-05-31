6da78281dd1b0098b625044503341e7f0352f9bb
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-05-31 15:00:29 -0400

    console log scheduler

--- src/reducers/scheduler-reducer.js
+++ src/reducers/scheduler-reducer.js
@@ -11,7 +11,7 @@ const SchedulerReducer = (state = initialState, action = {}) => {
       return { ...state, current: action.payload };
     case ActionTypes.JOIN_SCHEDULER:
       return { ...state, current: action.payload };
-    case ActionTypes.GET_SCHEDULER_ID:
+    case ActionTypes.GET_SCHEDULER:
       return { ...state, current: action.payload };
     case ActionTypes.GET_SCHEDULERS:
       return { ...state, all: action.payload };
