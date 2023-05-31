81340e38af4fe869de8a370902aca7f05296b8f9
MariaC27 <maria.h.cristoforo.24@dartmouth.edu>
2023-05-31 15:20:29 -0400

    changes to create page

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -18,35 +18,6 @@ function Main(props) {
   const [eventList, setEventList] = useState({});
   const maxAvail = 0;
 
-  // helper async function to create events
-  const makeEvent = async (item) => {
-    // console.log('time item:', item);
-    const newevent = await dispatch(createEvent(item));
-    if (newevent) {
-      return newevent;
-    }
-    return 0;
-  };
-
-  // create an empty calendar
-  const timeList = {};
-  const createCalendar = (start, end) => {
-    // for loop order is kind of funky because it is easier to change it in the DOM than manipulate it with CSS grid
-    for (let t = start; t < end + 1; t += 1) { // hours specified
-      for (let b = 0; b < 4; b += 1) { // 15 minute increments (0 is on the dot, 1 is 15m, 2 is 30m, 3 is 45m)
-        for (let d = 0; d < 7; d += 1) { // 7 days in week
-          const timeString = `${String(d)}.${String(t)}.${String(b)}`;
-          const timeItem = ({
-            key: timeString, day: d, time: t, block: b, count: 0, available: [],
-          });
-          const newEvent = makeEvent(timeItem);
-          timeList[timeString] = newEvent;
-        }
-      }
-    }
-    return timeList;
-  };
-
   // update current spaces on calendar based on user input
   const updateEvent = (id, fields) => { // modified from Chloe Fugle lab 3
     setEventList(
@@ -70,8 +41,8 @@ function Main(props) {
 
   // load the blank calendar, then load user events and style them
   async function loadCalendar() {
-    await setEventList(createCalendar(times.start, times.end));
-    console.log(eventList);
+    // await setEventList(createCalendar(times.start, times.end));
+    // console.log(eventList);
     await updateCalendar();
     return (maxAvail);
   }
